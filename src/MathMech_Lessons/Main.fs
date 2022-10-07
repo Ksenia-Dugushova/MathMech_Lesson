06932f1d7729cb93af282c734418daca2eaf6fdb
Ksenia-Dugushova <ksenia.dugushova.ru@mail.ru>
2022-10-07 16:39:12 +0400

    first

--- src/MathMech_Lessons/Main.fs
+++ src/MathMech_Lessons/Main.fs
@@ -1,98 +1,105 @@
-namespace MathMech_Lessons
+namespace MathMech_lessons
 
 open System.Reflection
 
-module AssemblyInfo =
-
-    let metaDataValue (mda: AssemblyMetadataAttribute) = mda.Value
-
-    let getMetaDataAttribute (assembly: Assembly) key =
-        assembly.GetCustomAttributes(typedefof<AssemblyMetadataAttribute>)
-        |> Seq.cast<AssemblyMetadataAttribute>
-        |> Seq.find (fun x -> x.Key = key)
-
-    let getReleaseDate assembly =
-        "ReleaseDate"
-        |> getMetaDataAttribute assembly
-        |> metaDataValue
-
-    let getGitHash assembly =
-        "GitHash"
-        |> getMetaDataAttribute assembly
-        |> metaDataValue
-
-    let getVersion assembly =
-        "AssemblyVersion"
-        |> getMetaDataAttribute assembly
-        |> metaDataValue
-
-    let assembly = lazy (Assembly.GetEntryAssembly())
-
-    let printVersion () =
-        let version = assembly.Force().GetName().Version
-        printfn "%A" version
-
-    let printInfo () =
-        let assembly = assembly.Force()
-        let name = assembly.GetName()
-        let version = assembly.GetName().Version
-        let releaseDate = getReleaseDate assembly
-        let githash = getGitHash assembly
-        printfn "%s - %A - %s - %s" name.Name version releaseDate githash
-
-module Say =
-    open System
-
-    let nothing name =
-        name
-        |> ignore
-
-    let hello name = sprintf "Hello %s" name
-
-    let colorizeIn (color: string) str =
-        let oldColor = Console.ForegroundColor
-        Console.ForegroundColor <- (Enum.Parse(typedefof<ConsoleColor>, color) :?> ConsoleColor)
-        printfn "%s" str
-        Console.ForegroundColor <- oldColor
-
 module Main =
-    open Argu
 
-    type CLIArguments =
-        | Info
-        | Version
-        | Favorite_Color of string // Look in App.config
-        | [<MainCommand>] Hello of string
+    //The first task. Exponentiation in a simple way
+    let f1 (x: float) (y: int) : float =
 
-        interface IArgParserTemplate with
-            member s.Usage =
-                match s with
-                | Info -> "More detailed information"
-                | Version -> "Version of application"
-                | Favorite_Color _ -> "Favorite color"
-                | Hello _ -> "Who to say hello to"
+        if
+            x = 0.0
+            && y = 0
+        then
+            printfn "Not defined"
+            0.0
+        elif x = 1.0 then
+            1.0
+        elif x = 0.0 then
+            0.0
+        else if y < 0 then
+            let mutable p = 1.0
+
+            for n in 0 .. -1 .. y do
+                p <-
+                    p
+                    * (1.0 / x)
+
+            p
+        elif y = 0 then
+            1.0
+        else
+            let mutable k = 1.0
+
+            for m in 1..y do
+                k <- k * x
+
+            k
+
+
+    //The second task. Exponentiation in a fast way
+    let rec f2 (x: float) (y: int) : float =
+        if
+            x = 0.0
+            && y = 0
+        then
+            printfn "Not defined"
+            0.0
+        elif x = 1.0 then
+            1.0
+        elif x = 0.0 then
+            0.0
+        else
+            let k =
+                if y = 0 then
+                    1.0
+                elif y % 2 = 1 then
+                    let m = f2 x (abs y / 2)
+                    m * m * x
+                else
+                    let m = f2 x (abs y / 2)
+                    m * m
+
+            if y > 0 then k else 1.0 / k
+
+
+    //The third task. Difference between array elements
+    let f3 (array: float array) =
+        if array = [||] then
+            failwith "Please enter several values into the array"
+
+        elif array.Length = 1 then
+            failwith "Please fill in the array with at least two elements"
+
+        else
+            let mutable max = array[0]
+            let mutable min = array[0]
+            //numbering from zero, so "-1"
+            for i = 1 to array.Length
+                         - 1 do
+                if array[i] > max then
+                    max <- array[i]
+
+                if array[i] < min then
+                    min <- array[i]
+
+            max - min
+
+
+    //The fourth task. Difference between array elements
+    let f4 (x: int) (y: int) =
+        let first_num = if x <= y then x else y
+        let second_num = if x <= y then y else x
+
+        if x % 2 = 0 then
+            let result: int array = [| for i in (x + 1) .. 2 .. (y - 1) -> i |]
+            result
+        else
+            let result: int array = [| for i in (x + 2) .. 2 .. (y - 1) -> i |]
+            result
 
     [<EntryPoint>]
-    let main (argv: string array) =
-        let parser = ArgumentParser.Create<CLIArguments>(programName = "MathMech_Lessons")
-        let results = parser.Parse(argv)
+    let main (argv: string array) = 0
 
-        if results.Contains Version then
-            AssemblyInfo.printVersion ()
-        elif results.Contains Info then
-            AssemblyInfo.printInfo ()
-        elif results.Contains Hello then
-            match results.TryGetResult Hello with
-            | Some v ->
-                let color = results.GetResult Favorite_Color
 
-                Say.hello v
-                |> Say.colorizeIn color
-            | None ->
-                parser.PrintUsage()
-                |> printfn "%s"
-        else
-            parser.PrintUsage()
-            |> printfn "%s"
-
-        0
+    
--- tests/MathMech_Lessons.Tests/Tests.fs
+++ tests/MathMech_Lessons.Tests/Tests.fs
@@ -1,18 +1,133 @@
-namespace MathMech_Lessons.Tests
+namespace MathMech_lessons.Tests
 
 open Expecto
-open MathMech_Lessons
+open MathMech_lessons.Main
 
 module SayTests =
     [<Tests>]
     let tests =
         testList "samples" [
-            testCase "Say nothing"
+            testCase "1 test for exponentiation in a simple way"
             <| fun _ ->
-                let subject = Say.nothing ()
-                Expect.equal subject () "Not an absolute unit"
-            testCase "Say hello all"
+                let actualResult = f1 2 10
+                Expect.equal actualResult 1024 "The answer isn't correct"
+
+            testCase "2 test for exponentiation in a simple way"
             <| fun _ ->
-                let subject = Say.hello "all"
-                Expect.equal subject "Hello all" "You didn't say hello"
+                let actualResult = f1 0 5
+                Expect.equal actualResult 0 "The answer isn't correct"
+
+            testCase "3 test for exponentiation in a simple way"
+            <| fun _ ->
+                let actualResult = f1 1 5
+                Expect.equal actualResult 1 "The answer isn't correct"
+
+            testCase "4 test for exponentiation in a simple way"
+            <| fun _ ->
+                let actualResult = f1 -2 5
+                Expect.equal actualResult -32 "The answer isn't correct"
+
+
+            testCase "1 test for exponentiation in a fast way"
+            <| fun _ ->
+                let actualResult = f2 2 10
+                Expect.equal actualResult 1024 "The answer isn't correct"
+
+            testCase "2 test for exponentiation in a fast way"
+            <| fun _ ->
+                let actualResult = f2 0 5
+                Expect.equal actualResult 0 "The answer isn't correct"
+
+            testCase "3 test for exponentiation in a fast way"
+            <| fun _ ->
+                let actualResult = f2 1 5
+                Expect.equal actualResult 1 "The answer isn't correct"
+
+            testCase "4 test for exponentiation in a fast way"
+            <| fun _ ->
+                let actualResult = f2 -2 5
+                Expect.equal actualResult -32 "The answer isn't correct"
+
+
+            testCase "1 test for difference"
+            <| fun _ ->
+                let actualResult =
+                    f3 [|
+                        1
+                        2
+                        3
+                    |]
+
+                Expect.equal actualResult 2 "The answer isn't correct"
+
+            testCase "2 test for difference"
+            <| fun _ ->
+                let actualResult =
+                    f3 [|
+                        0
+                        0
+                        0
+                    |]
+
+                Expect.equal actualResult 0 "The answer isn't correct"
+
+            testCase "3 test for difference"
+            <| fun _ ->
+                let actualResult =
+                    f3 [|
+                        -1
+                        2
+                        -13
+                        9
+                    |]
+
+                Expect.equal actualResult 22 "The answer isn't correct"
+
+
+            testCase "1 test for array"
+            <| fun _ ->
+                let actualResult = f4 1 5
+                Expect.equal actualResult [| 3 |] "The answer isn't correct"
+
+            testCase "2 test for array"
+            <| fun _ ->
+                let actualResult = f4 5 13
+
+                Expect.equal
+                    actualResult
+                    [|
+                        7
+                        9
+                        11
+                    |]
+                    "The answer isn't correct"
+
+            testCase "3 test for array"
+            <| fun _ ->
+                let actualResult = f4 2 14
+
+                Expect.equal
+                    actualResult
+                    [|
+                        3
+                        5
+                        7
+                        9
+                        11
+                        13
+                    |]
+                    "The answer isn't correct"
+            testCase "4 test for array"
+            <| fun _ ->
+                let actualResult = f4 -5 3
+
+                Expect.equal
+                    actualResult
+                    [|
+                        -3
+                        -1
+                        1
+                    |]
+                    "The answer isn't correct"
+
         ]
