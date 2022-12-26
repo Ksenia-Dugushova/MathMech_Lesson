fa07483a1bfb4272704daf2ece29bd7faa18480b
Ksenia-Dugushova <ksenia.dugushova.ru@mail.ru>
2022-12-26 16:19:59 +0400

    changes

--- src/MathMech_Lessons/MultiMatrix.fs
+++ src/MathMech_Lessons/MultiMatrix.fs
@@ -19,15 +19,15 @@ let multiplication plus (multiOperation: option<'value1> -> option<'value2> -> o
             let fst =
                 (addVector
                  <| plus
-                 <| SparseVector(multiTree left fst, vector.Length, vector.LengthSquare)
-                 <| SparseVector(multiTree right thd, vector.Length, vector.LengthSquare))
+                 <| SparseVector(multiTree left fst, vector.Length)
+                 <| SparseVector(multiTree right thd, vector.Length))
                     .Storage
 
             let snd =
                 (addVector
                  <| plus
-                 <| SparseVector(multiTree left snd, vector.Length, vector.LengthSquare)
-                 <| SparseVector(multiTree right fth, vector.Length, vector.LengthSquare))
+                 <| SparseVector(multiTree left snd, vector.Length)
+                 <| SparseVector(multiTree right fth, vector.Length))
                     .Storage
 
             if fst = BinaryTree.None && snd = BinaryTree.None then
@@ -54,10 +54,13 @@ let multiplication plus (multiOperation: option<'value1> -> option<'value2> -> o
         else
             tree
 
+    let powerSizeV = ceilPowTwo vector.Length
+    let powerSizeM = ceilPowTwo (max matrix.RowCount matrix.ColumnCount)
+
     if vector.Length = matrix.RowCount then
         let makeTree =
-            if vector.LengthSquare <> matrix.LengthSquare then
-                multiTree (binareTreeMaker vector.Storage matrix.LengthSquare vector.LengthSquare) matrix.Storage
+            if powerSizeV <> powerSizeM then
+                multiTree (binareTreeMaker vector.Storage powerSizeM powerSizeV) matrix.Storage
             else
                 multiTree vector.Storage matrix.Storage
 
@@ -65,10 +68,10 @@ let multiplication plus (multiOperation: option<'value1> -> option<'value2> -> o
             if matrix.RowCount > matrix.ColumnCount then
                 let desiredSize = int (2.0 ** ceil (Math.Log(matrix.ColumnCount, 2)))
 
-                CutBinaryTree makeTree desiredSize matrix.LengthSquare
+                CutBinaryTree makeTree desiredSize powerSizeM
             else
                 makeTree
 
-        SparseVector(splitTree, vector.Length, vector.Length)
+        SparseVector(splitTree, vector.Length)
     else
         failwith "Something went wrong"
